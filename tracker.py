0ea2cdaaab2192fd7b8bf3c60e5c2a33f87922e0
Kyle <kyle.stephens@ucdenver.edu>
2023-08-29 14:13:45 -0600

    opening window start

---
+++ tracker.py
@@ -0,0 +1,13 @@
+#imports
+import tkinter as tk
+
+#making gui 
+#intro
+window = tk.Tk()
+opening = tk.Label(text="Welcome! \
+                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~\
+                   Launching Program") # come back later and have the text stack one over the other
+                                       # and have it be centered
+opening.pack()
+window.mainloop()
+
